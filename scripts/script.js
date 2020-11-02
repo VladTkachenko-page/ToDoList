fd20e0fe0953cba60376251d492b6aa39958ea3b
Vlad Tkachenko <vladtka@ukr.net>
2020-11-02 12:39:11 +0200

    feat (script.js): new feat

---
+++ scripts/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+ const todoControl = document.querySelector('.todo-control'),
+       headerInput = document.querySelector('.header-input'),
+       todoList = document.querySelector('.todo-list'),
+       todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [];              
+todoData = JSON.parse(localStorage.getItem('array'));  
+
+const render = function() {
+  todoList.textContent = '';
+  todoCompleted.textContent = '';
+  localStorage.setItem('array', JSON.stringify(todoData));
+  
+    todoData.forEach(function(item) {
+      
+      const li = document.createElement('li');
+      li.classList.add('todo-item');
+
+      li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+      '<div class="todo-buttons">' +
+        '<button class="todo-remove"></button>' +
+        '<button class="todo-complete"></button>' +
+      '</div>';
+
+      if (item.completed) {
+        todoCompleted.append(li);
+      } else {
+        todoList.append(li);
+      };
+
+      const btnTodoComplete = li.querySelector('.todo-complete');
+      btnTodoComplete.addEventListener('click', function() {
+        item.completed = !item.completed;
+        render();
+      });
+      const btnTodoRemove = li.querySelector('.todo-remove');
+      btnTodoRemove.addEventListener('click', function() {
+        todoData.splice(item, 1);
+        render();
+      });
+    });
+};
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const newTodo = {
+      value: headerInput.value.trim(),
+      completed: false
+    };
+
+    if (headerInput.value.trim() === '') {
+      headerInput.value = '';
+      return false;
+    } else {
+      todoData.push(newTodo);
+      render();
+      headerInput.value = '';
+    }
+  });
+  render();
